445d870ba11a272ba9867c774457c7b7f033659b
dongjin5478 <dongjinmaster9@gmail.com>
2022-09-27 11:17:51 +0530

    swagger UI version updated

--- src/main/java/com/rest/api/RestapiApplication.java
+++ src/main/java/com/rest/api/RestapiApplication.java
@@ -2,12 +2,26 @@ package com.rest.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import com.google.gson.Gson;
 
 @SpringBootApplication
 public class RestapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestapiApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+	@Bean
+	public Gson getGson() {
+		return new Gson();
 	}
 
 }
