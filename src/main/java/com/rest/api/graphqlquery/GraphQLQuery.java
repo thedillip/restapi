9df077e02f479735d87369a164516e698b9e531f
dongjin5478 <dongjinmaster9@gmail.com>
2022-10-18 22:16:06 +0530

    new changes.

--- src/main/java/com/rest/api/graphqlquery/GraphQLQuery.java
+++ src/main/java/com/rest/api/graphqlquery/GraphQLQuery.java
@@ -5,6 +5,8 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
+import com.rest.api.response.BankDetailsResponse;
+import com.rest.api.service.CommonService;
 import com.rest.api.service.GraphQLQueryService;
 
 @Controller
@@ -12,9 +14,16 @@ public class GraphQLQuery {
 
 	@Autowired
 	private GraphQLQueryService graphQLQueryService;
+	@Autowired
+	private CommonService commonService;
 
 	@QueryMapping(value = "message")
 	public String helloMessage(@Argument(name = "name") String name) {
 		return graphQLQueryService.helloMessage(name);
+	}
+
+	@QueryMapping(value = "bankDetails")
+	public BankDetailsResponse getBankDetailsByIfsc(@Argument(name = "ifsc") String ifsc) {
+		return commonService.getBankDetailsByIfsc(ifsc);
 	}
 }
